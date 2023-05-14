a5b376c3ab80cdcf7eaaed09c56adf5e63cdf486
Peipei Wu <peipeiwu1996@gmail.com>
2023-05-14 11:10:21 +0100

    add metrics and losses

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ data = data_module.T3DataModule(batch_size=1)
 data.setup()
 dataloader = data.train_dataloader()
 
-# samples = next(iter(dataloader)) 
-# feats, labels = samples
+samples = next(iter(dataloader)) 
+feats, labels = samples
 
-feats, labels = data.train_set.__getitem__(150)
+# feats, labels = data.train_set.__getitem__(150)
 
 model = T3Model()
 target_cls,doa,noise = model(feats)
